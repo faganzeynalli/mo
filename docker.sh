35de9722b951d8c842cd215b830fecf19e9b6022
faganzeynalli <fagan.zeynalli@gmail.com>
2025-02-24 23:58:21 +0400

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -414,7 +414,7 @@ run-installer() {
     fi
   fi
 
-  curl -fsSL https://raw.githubusercontent.com/AzuraCast/faganzeynalli/mo/main/docker-compose.installer.yml -o docker-compose.installer.yml
+  curl -fsSL https://raw.githubusercontent.com/faganzeynalli/mo/main/docker-compose.installer.yml -o docker-compose.installer.yml
 
   dc -p azuracast_installer -f docker-compose.installer.yml pull
   dc -p azuracast_installer -f docker-compose.installer.yml run --rm installer install "$@"

44c99173078b06d86a058ef804b3999c5fe56a9a
faganzeynalli <fagan.zeynalli@gmail.com>
2025-02-24 23:40:57 +0400

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -396,7 +396,7 @@ run-installer() {
   AZURACAST_RELEASE_BRANCH=$(get-release-branch-name)
 
   if [[ ! -f .env ]]; then
-    curl -fsSL https://raw.githubusercontent.com/AzuraCast/AzuraCast/$AZURACAST_RELEASE_BRANCH/sample.env -o .env
+    curl -fsSL https://raw.githubusercontent.com//faganzeynalli/mo/main/sample.env -o .env
   fi
   if [[ ! -f azuracast.env ]]; then
     curl -fsSL https://raw.githubusercontent.com/AzuraCast/AzuraCast/$AZURACAST_RELEASE_BRANCH/azuracast.sample.env -o azuracast.env
